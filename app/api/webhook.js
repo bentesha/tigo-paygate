6f20f779060471f2b20277e14589a00a7940a96b
bentesha <benedict.tesha@jamaatech.com>
2019-06-21 08:16:28 +0300

    jsdocs improvement for webhook callback function

--- app/api/webhook.js
+++ app/api/webhook.js
@@ -4,9 +4,19 @@ const codes = require('./response-codes')
 const debug = require('debug')('tigoapi')
 
 /**
+ * A function to be called with result of the customer charge request
+ * @callback callbackFn
+ * @param {object} result - An object containing result of the customer charge request
+ * @param {boolean} result.status - True if the request was successful
+ * @param {string} result.transactionId - Transaction ID
+ * @param {number} result.amount - The amount requested the customer be charged
+ * @param {string} result.reference - The reference associated with the charge request
+ * @return {boolean} True to confirm transaction or false to cancel transaction
+ */
+
+/**
  * TigoPesa webhook middleware handler
- * @param {import('..')} app - Application instance
- * @param {function} callback - A function that will be called with reference and amount for
+ * @param {callbackFn} callback - A function that will be called with reference and amount for
  * successful transctions. Should return true to confirm transction. Otherwise transaction will be rejected
  */
 module.exports = callback => ({ body }, response, next) => {
